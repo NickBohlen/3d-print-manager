23c16ff42bd4dbce67ba292d68ae886b4d5fcfef
NickBohlen <nbohlen@unomaha.edu>
2024-12-14 15:43:25 -0600

    final

--- print_manager/print_manager/settings.py
+++ print_manager/print_manager/settings.py
@@ -123,8 +123,6 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
 # Media files configuration (for HLS streaming)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'print_manager/media')
