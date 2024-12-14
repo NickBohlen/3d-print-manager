2ba24da081c67fc79dd987d34d2e030bb2c0c4c4
NickBohlen <nbohlen@unomaha.edu>
2024-12-14 12:05:17 -0600

    add docker

--- print_manager/print_manager/settings.py
+++ print_manager/print_manager/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Media files configuration (for HLS streaming)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'print_manager/media')
